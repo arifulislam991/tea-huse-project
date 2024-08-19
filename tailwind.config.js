784e43eea27a55936ec04d3474067836a7768c92
arifulislam991 <arifulislam99101@gmail.com>
2024-08-19 11:47:40 +0600

    header section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
       extend: {
         colors: {
           clifford: '#da373d',
-        }
+          boldText: "#1C1C1C",
+          normalText: "#777777"
+        },
       }
     },
     plugins: [],
